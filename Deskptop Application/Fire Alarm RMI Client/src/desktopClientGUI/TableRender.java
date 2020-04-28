2e269a88892da1097cef2c6637ae9870513e3107
pawanpiumal <pawanpiumal1@gmail.com>
2020-04-28 13:35:02 +0530

    Desktop Client Table Error Changed

--- Deskptop Application/Fire Alarm RMI Client/src/desktopClientGUI/TableRender.java
+++ Deskptop Application/Fire Alarm RMI Client/src/desktopClientGUI/TableRender.java
@@ -23,18 +23,22 @@ class TableRender extends DefaultTableCellRenderer {
 		float backgroudnColorHSBYello[] = Color.RGBtoHSB(168, 170, 65, null);
 		float backgroudnColorHSBRed[] = Color.RGBtoHSB(170, 65, 65, null);
 		float backgroudnColorHSBPink[] = Color.RGBtoHSB(192, 169, 166, null);
-		if(((String)table.getValueAt(row, 1)).equals("Not Active")) {
+		if (((String) table.getValueAt(row, 1)).equals("Not Active")) {
 			setForeground(Color.black);
-			setBackground(Color.getHSBColor(backgroudnColorHSBPink[0], backgroudnColorHSBPink[1], backgroudnColorHSBPink[2]));
-		}else if ((Integer) table.getValueAt(row, 5) < 5 && (Integer) table.getValueAt(row, 4) < 5) {
+			setBackground(
+					Color.getHSBColor(backgroudnColorHSBPink[0], backgroudnColorHSBPink[1], backgroudnColorHSBPink[2]));
+		} else if ((Integer) table.getValueAt(row, 5) < 5 && (Integer) table.getValueAt(row, 4) < 5) {
 			setForeground(Color.black);
-			setBackground(Color.getHSBColor(backgroudnColorHSBGreen[0], backgroudnColorHSBGreen[1], backgroudnColorHSBGreen[2]));
+			setBackground(Color.getHSBColor(backgroudnColorHSBGreen[0], backgroudnColorHSBGreen[1],
+					backgroudnColorHSBGreen[2]));
+		} else if ((Integer) table.getValueAt(row, 5) > 5 || (Integer) table.getValueAt(row, 4) > 5) {
+			setBackground(
+					Color.getHSBColor(backgroudnColorHSBRed[0], backgroudnColorHSBRed[1], backgroudnColorHSBRed[2]));
+			setForeground(Color.black);
 		} else if ((Integer) table.getValueAt(row, 5) == 5 || (Integer) table.getValueAt(row, 4) == 5) {
 			setForeground(Color.black);
-			setBackground(Color.getHSBColor(backgroudnColorHSBYello[0], backgroudnColorHSBYello[1], backgroudnColorHSBYello[2]));
-		} else if((Integer) table.getValueAt(row, 5) > 5 || (Integer) table.getValueAt(row, 4) > 5){
-			setBackground(Color.getHSBColor(backgroudnColorHSBRed[0], backgroudnColorHSBRed[1], backgroudnColorHSBRed[2]));
-			setForeground(Color.black);
+			setBackground(Color.getHSBColor(backgroudnColorHSBYello[0], backgroudnColorHSBYello[1],
+					backgroudnColorHSBYello[2]));
 		}
 		setText(value != null ? value.toString() : "");
 		return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
